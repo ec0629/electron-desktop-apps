935c266c2fae7facca7569098ac5fff229c4c10e
Jeff Simonitto <jsimonitto@gmail.com>
2020-12-06 13:37:06 -0500

    IPC communication using electron

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
@@ -10,4 +11,10 @@ app.on('ready', () => {
     },
   });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log('File length is:', metadata.format.duration);
+  });
 });
