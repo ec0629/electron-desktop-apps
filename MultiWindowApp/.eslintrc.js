9d228d08362a84176aa8a140d2b182e2b962bd9f
Jeff Simonitto <jsimonitto@gmail.com>
2020-12-06 15:09:54 -0500

    setup for new project

--- MultiWindowApp/.eslintrc.js
+++ MultiWindowApp/.eslintrc.js
@@ -1,22 +1,16 @@
 module.exports = {
   env: {
     browser: true,
+    commonjs: true,
     es2021: true,
+    node: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    'airbnb-base',
   ],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 12,
-    sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
   rules: {
   },
 };
