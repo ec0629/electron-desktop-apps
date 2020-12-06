0e303c4aa617fc32bf21be475c834c6d8b1c17e1
Jeff Simonitto <jsimonitto@gmail.com>
2020-12-06 18:54:08 -0500

    Add quit option to Menu

--- MultiWindowApp/index.js
+++ MultiWindowApp/index.js
@@ -4,14 +4,29 @@ const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
+const isMac = process.platform === 'darwin';
+
 const menuTemplate = [
   {
     label: 'File',
     submenu: [
       { label: 'New Todo' },
+      {
+        label: 'Quit',
+        accelerator: isMac
+          ? 'Command+Q'
+          : 'Ctrl+Q',
+        click() {
+          app.quit();
+        },
+      },
     ],
   },
 ];
+
+if (isMac) {
+  menuTemplate.unshift({});
+}
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow();
